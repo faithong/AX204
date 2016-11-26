12dfa1f92579a8265208407c914c6cf0da11fa33
Faith <faithong02@gmail.com>
2016-11-26 12:51:52 +0800

    4th

--- phaserman/app.js
+++ phaserman/app.js
@@ -6,7 +6,7 @@ var life = 3;
 
 function preload() {
 	game.load.image('sky','assets/sky.png');
-	game.load.image('ground','assets/ground.png');
+	game.load.image('ground','assets/platform.png');
 	game.load.image('star','assets/star.png');
 	game.load.spritesheet('dude','assets/dude.png', 32, 48);
 	game.load.spritesheet('assets/baddie.png', 32, 32); //always tell how tall and wide each frame is for spritesheets
