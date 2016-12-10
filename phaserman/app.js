fb3cb3b993e0e3e117d379bff0a37476e1fdaccf
Faith <faithong02@gmail.com>
2016-12-10 11:41:48 +0800

    fixed bugs

--- phaserman/app.js
+++ phaserman/app.js
@@ -28,7 +28,7 @@ function create() {
 	//ledges
 	var ledge = platforms.create(400, 400, 'ground');
 	ledge.body.immovable = true;
-	ledge = platforms.create(-150, 250, 'ground');
+	ledge = platforms.create(-160, 250, 'ground');
 	ledge.body.immovable = true;
 
 	//Player 
